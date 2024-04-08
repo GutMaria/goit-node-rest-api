270673ea2087306da48c4ec2d48d318ff7ebca8a
GutMaria <gootmaria@gmail.com>
2024-04-08 14:13:12 +0300

    add file to mongoDB

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,6 +5,7 @@ import {
   getCurrent,
   logout,
   updateSubscription,
+  updateAvatar,
 } from "../controllers/userControllers.js";
 import {
   userRegisterSchema,
@@ -12,6 +13,7 @@ import {
 } from "../schemas/usersSchemas.js";
 import validateBody from "../decorators/validateBody.js";
 import autenticate from "../middlwares/authenticate.js";
+import upload from "../middlwares/upload.js";
 
 const userRouter = express.Router();
 
@@ -20,5 +22,12 @@ userRouter.post("/login", validateBody(userLoginSchema), login);
 userRouter.post("/logout", autenticate, logout);
 userRouter.get("/current", autenticate, getCurrent);
 userRouter.patch("/", autenticate, updateSubscription);
+userRouter.patch(
+  "/avatars",
+  autenticate,
+  upload.single("avatar"),
+
+  updateAvatar
+);
 
 export default userRouter;
